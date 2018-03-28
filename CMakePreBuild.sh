e44cc654bc6f0921f8541d893da0c327c87f2957
Ubuntu <ubuntu@ip-172-31-15-21.us-west-2.compute.internal>
2018-03-28 22:28:19 +0000

    remove non-ND associations

--- CMakePreBuild.sh
+++ CMakePreBuild.sh
@@ -47,7 +47,7 @@ if [ $BUILD_PYTORCH = "ON" ] || [ $BUILD_PYTORCH = "YES" ] || [ $BUILD_PYTORCH =
 	# pip 9.0.1 from /home/ubuntu/.local/lib/python2.7/site-packages (python 2.7)
 
 	# setproctitle extension used by A3G
-	sudo pip install setproctitle 
+	# sudo pip install setproctitle 
 
 	# install numpy
 	sudo pip install numpy
